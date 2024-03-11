d8faedf7941f4b5465e465466c1967f0c38d3ab2
federico-dibitonto <f.dibitonto94@gmail.com>
2024-03-11 18:06:12 +0100

    create data.js and store.js file

---
+++ src/js/data.js
@@ -0,0 +1,357 @@
+import {reactive} from 'vue'
+
+export const data = reactive ({
+    
+    genLink: [
+        {
+            link: 'home'
+        },
+        {
+            link: 'pages'
+        },
+        {
+            link: 'tournament'
+        },
+        {
+            link: 'shop'
+        },
+        {
+            link: 'blog'
+        },
+        {
+            link: 'contact'
+        }
+    ],
+
+    social: [
+        {
+            soc: 'image (4).png'
+        },
+        {
+            soc: 'image (5).png'
+        },
+        {
+            soc: 'image (6).png'
+        },
+        {
+            soc: 'image (7).png'
+        }
+    ],
+
+    topFloat: [
+        {
+            img: '1.png'
+        },
+        {
+            img: '2.png'
+        },
+        {
+            img: '3.png'
+        },
+        {
+            img: '4.png'
+        },
+        {
+            img: '5.png'
+        },
+        {
+            img: '6.png'
+        }
+    ],
+
+    comp1: [
+        {
+            img: 'exprince.png',
+            subtitle: '12 Year Experience',
+            title: 'We Are The Best Gaming Company',
+            text: 'Nulla quis lorem ut libero malesuada feugiat. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Cras ultricies ligula sed magna dictum porta.Praesent sapien massa, convallis a pellentesque nec, egestas non nisi praesent sapien massa ligula sed magna dictum porta. Nulla quis lorem ut libero malesuada feugiat. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Cras ultricies ligulaa.',
+            
+            bullet: [
+                {
+                    bull1: 'Sed porttitor lectus nibh. Proin eget tortor risus.'
+                },
+                {
+                    bull2: 'Praesent sapien massa, convallis a pellentesque nec. risus.'
+                },
+                {
+                    bull3: 'Praesent sapien massa, convallis a pellentesque nec. risus.'
+                },
+                {
+                    bull4: 'Praesent sapien massa, convallis a pellentesque nec. risus.'
+                }
+            ]
+        }
+    ],
+
+    comp2: [
+        {
+            subtitle: 'Our Extrem Feature',
+            title: 'The Best Gaming Arena On The World!',
+            text: 'Nulla quis lorem ut libero malesuada feugiat. Vivamus magna justo, lacinia eget consectetur sed, at tellus. Cras ultricies ligula sed magna ivamus magna justo, dictum porta. sapien massa, convallis.',
+            
+            box: [
+                {
+                    img: 'feature1.png',
+                    title: 'Live Streaming',
+                    text: 'Nulla porttitor accumsan tincidunt. Pellentesque in.'
+                },
+                {
+                    img: 'feature2.png',
+                    title: 'Gaming News',
+                    text: 'Nulla porttitor accumsan tincidunt. Pellentesque in.'
+                },
+                {
+                    img: 'feature3.png',
+                    title: 'Great Tournament',
+                    text: 'Nulla porttitor accumsan tincidunt. Pellentesque in.'
+                },
+                {
+                    img: 'feature4.png',
+                    title: 'Awward Ceremony',
+                    text: 'Nulla porttitor accumsan tincidunt. Pellentesque in.'
+                }
+            ]
+        }
+    ],
+
+    comp3: [
+        {
+            subtitle: 'Live Streaming',
+            title: 'Checkout Our Live Streaming',
+            
+            box: [
+                {
+                    img: 'v-small1.png',
+                    title: 'Call Of Deauty',
+                    text: 'Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.'
+                },
+                {
+                    img: 'v-small2.png',
+                    title: 'Gaming News',
+                    text: 'Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.'
+                }
+            ]
+        }
+    ],
+
+    comp4: [
+        {
+            subtitle: 'Trending Battle',
+            title: 'Upcoming Match',
+            
+            // box: [
+            //     {
+            //         img: 'feature1.png',
+            //         title: 'Live Streaming',
+            //         text: 'Nulla porttitor accumsan tincidunt. Pellentesque in.'
+            //     },
+            //     {
+            //         img: 'feature2.png',
+            //         title: 'Gaming News',
+            //         text: 'Nulla porttitor accumsan tincidunt. Pellentesque in.'
+            //     },
+            //     {
+            //         img: 'feature3.png',
+            //         title: 'Great Tournament',
+            //         text: 'Nulla porttitor accumsan tincidunt. Pellentesque in.'
+            //     },
+            //     {
+            //         img: 'feature4.png',
+            //         title: 'Awward Ceremony',
+            //         text: 'Nulla porttitor accumsan tincidunt. Pellentesque in.'
+            //     }
+            // ]
+        }
+    ],
+
+    comp5: [
+        {
+            subtitle: 'Our Extrem Feature',
+            title: 'Choose Who Is The Best In World!',
+            
+            box: [
+                {
+                    img: 'protfolio1.png',
+                    title: 'Assassin \'s Creed',
+                    text: 'Donec sollicitudin malesuada.'
+                },
+                {
+                    img: 'protfolio2.png',
+                    title: 'Call Of Deauty',
+                    text: 'Donec sollicitudin malesuada.'
+                },
+                {
+                    img: 'protfolio3.png',
+                    title: 'Tomb Rider',
+                    text: 'Donec sollicitudin malesuada.'
+                },
+                {
+                    img: 'protfolio4.png',
+                    title: 'Mortal Combat X',
+                    text: 'Donec sollicitudin malesuada.'
+                }
+            ]
+        }
+    ],
+
+    comp6: [
+        {
+            subtitle: 'Upcoming Match',
+            title: 'Keep Eyes & Manage upcoming Battle',
+            
+            box: [
+                {
+                    img: '1.png',
+                    title: 'Ninja Warrior Gaming',
+                    vote: '9.45',
+                    data: '1st November,2022'
+                },
+                {
+                    img: '5.png',
+                    title: 'Witch Gaming',
+                    vote: '9.45',
+                    data: '1st November,2022'
+                },
+                {
+                    img: '3.png',
+                    title: 'SkullG Gaming',
+                    vote: '9.45',
+                    data: '1st November,2022'
+                }
+            ]
+        }
+    ],
+
+    comp7: [
+        {
+            subtitle: 'Team Members',
+            title: 'Our Experts Team Members',
+            
+            box: [
+                {
+                    img: 'team1.png',
+                    name: 'Cristiano Messi',
+                    rule: 'CEO/Founder'
+                },
+                {
+                    img: 'team2.png',
+                    name: 'Sergio Naymer',
+                    rule: 'Programmer'
+                },
+                {
+                    img: 'team3.png',
+                    name: 'Antonella Roccuzzo',
+                    rule: 'Project Manager'
+                },
+                {
+                    img: 'team4.png',
+                    name: 'Lionel Ramos',
+                    rule: 'Game Designer'
+                }
+            ]
+        }
+    ],
+
+    comp8: [
+        {
+            subtitle: 'Testimonials',
+            title: 'What Our Client Say',
+            
+            box: [
+                {
+                    img: 'test1.png',
+                    text: 'Donec sollicitudin molestie malesuada. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Cras ultricies ligula sed magna dictum porta. Donec rutrum congue leo eget',
+                    name: 'Mohammad Sala',
+                    rule: 'CEO/Founder'
+                },
+                {
+                    img: 'test2.png',
+                    text: 'Donec sollicitudin molestie malesuada. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Cras ultricies ligula sed magna dictum porta. Donec rutrum congue leo eget',
+                    name: 'Mohammad Sala',
+                    rule: 'CEO/Founder'
+                },
+                {
+                    img: 'test3.png',
+                    text: 'Donec sollicitudin molestie malesuada. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Cras ultricies ligula sed magna dictum porta. Donec rutrum congue leo eget',
+                    name: 'Mohammad Sala',
+                    rule: 'CEO/Founder'
+                },
+                {
+                    img: 'test4.png',
+                    text: 'Donec sollicitudin molestie malesuada. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Cras ultricies ligula sed magna dictum porta. Donec rutrum congue leo eget',
+                    name: 'Mohammad Sala',
+                    rule: 'CEO/Founder'
+                }
+            ]
+        }
+    ],
+
+    comp9: [
+        {
+            subtitle: 'Gammer Shop',
+            title: 'Explore Our Shop',
+            
+            box: [
+                {
+                    img: 's1.png',
+                    name: 'Asus Rog Strix A53',
+                    price: '$999.00',
+                    discount: '970.00'
+                },
+                {
+                    img: 's2.png',
+                    name: 'Asus Rog Strix A53',
+                    price: '$999.00',
+                    discount: '970.00'
+                },
+                {
+                    img: 's3.png',
+                    name: 'Asus Rog Strix A53',
+                    price: '$999.00',
+                    discount: '970.00'
+                },
+                {
+                    img: 's4.png',
+                    name: 'Asus Rog Strix A53',
+                    price: '$999.00',
+                    discount: '970.00'
+                }
+            ]
+        }
+    ],
+
+    comp10: [
+        {
+            subtitle: 'Latest News',
+            title: 'Stay Updated With our Latest News Articles',
+            
+            box: [
+                {
+                    img: 'b1.png',
+                    name: 'Ninja Warrior Gaming',
+                    writer: 'By Admin',
+                    data: 'Oct 25, 2022'
+                },
+                {
+                    img: 'b2.png',
+                    name: 'Gost Rider Returns',
+                    writer: 'By Admin',
+                    data: 'Oct 25, 2022'
+                },
+                {
+                    img: 'b3.png',
+                    name: 'Call Of Deauty 4',
+                    writer: 'By Admin',
+                    data: 'Oct 25, 2022'
+                },
+                {
+                    img: 'b4.png',
+                    name: 'Mortal Combat X',
+                    writer: 'By Admin',
+                    data: 'Oct 25, 2022'
+                }
+            ]
+        }
+    ]
+
+});
