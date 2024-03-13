4946e7d88439d81c10c680aa8804b22ea9b383b7
federico-dibitonto <f.dibitonto94@gmail.com>
2024-03-13 17:22:20 +0100

    fixed style and hover element

--- src/components/4-Commons/AppLink.vue
+++ src/components/4-Commons/AppLink.vue
@@ -9,11 +9,18 @@ export default {
 </script>
 
 <template>
-    <li class="nav-item">
-        <a class="nav-link active text-white text-uppercase" aria-current="page" href="#"><h5>{{ getLink.link }}</h5></a>
+    <li class="nav-item dropdown">
+        <a class="nav-link active text-white text-uppercase my_hover_link" aria-current="page" href="#"role="button" data-bs-toggle="dropdown" aria-expanded="false"><h5>{{ getLink.link }}</h5></a>
+        <ul class="dropdown-menu p-2 my_comp_bg_blu">
+            <li><a class="dropdown-item my_color_primary_green my_link_line" href="#">About us</a></li>
+            <li><a class="dropdown-item my_color_primary_green my_link_line" href="#">Matches</a></li>
+            <li><a class="dropdown-item my_color_primary_green" href="#">Team</a></li>
+        </ul>
     </li>
 </template>
 
 <style lang="scss">
-    
+    .my_link_line {
+        border-bottom: 1px solid rgb(67, 65, 65);
+    }
 </style>
